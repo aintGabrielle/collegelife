176d440555becefa0d96dfbdc6159514fe3ee030
aintGabrielle <109278729+aintGabrielle@users.noreply.github.com>
2022-09-21 10:37:31 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let ariane = document.getElementById('ariane')
+let jana = document.getElementById('jana')
+let mj = document.getElementById('mj')
+let joshua = document.getElementById('mallete')
+let jhon = document.getElementById('lloyd')
+
+let Mj = document.getElementById('ma')
+let Ariane = document.getElementById('ar')
+let Jana = document.getElementById('ja')
+let Joshua = document.getElementById('jo')
+let Jhon = document.getElementById('jh')
+
+ariane.addEventListener('click', () => {
+    window.location="https://www.facebook.com/profile.php?id=100078444624521"
+})
+jana.addEventListener('click', () => {
+    window.location="https://www.facebook.com/janalim15"
+})
+mj.addEventListener('click', () => {
+    window.location="https://www.facebook.com/obscureee14"
+})
+jhon.addEventListener('click', () => {
+    window.location="https://www.facebook.com/lloydiiiii28"
+    console.log("Click");
+})
+joshua.addEventListener('click', () => {
+    window.location="https://www.facebook.com/malletejoshua29"
+})
+
+Mj.addEventListener('click', () => {
+    window.location="aboutMj.html"
+})
+Ariane.addEventListener('click', () => {
+    window.location="aboutAriane.html"
+})
+Jana.addEventListener('click', () => {
+    window.location="aboutJana.html"
+})
+Joshua.addEventListener('click', () => {
+    window.location="aboutJoshua.html"
+})
+Jhon.addEventListener('click', () => {
+    window.location="aboutJhon.html"
+})
